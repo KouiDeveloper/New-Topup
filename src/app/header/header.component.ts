ac5ed54787ccd8eab51a2d9916f2222f7d0b36df
kouiDeveloper <kouiprogramer@gmail.com>
2018-05-02 11:03:41 +0700

    update and rearrange typescript and html

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -112,18 +112,12 @@ export class HeaderComponent implements OnInit {
   }
 /// INIT FUNCTIONS
 
-
-
   /// *************RECEIVING  */
   readClient(c): any {
     // this._client
     try {
       if (c !== undefined) {
         this._client = c;
-        // console.log('return from server');
-        // console.log(msg);
-        // console.log(this._client.data['command'] + this._client.data['command2']);
-        // alert(JSON.stringify(c));
         switch (this._client.data['command']) {
           case 'heart-beat':
             if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
@@ -136,21 +130,6 @@ export class HeaderComponent implements OnInit {
           case 'ping':
             console.log('ping OK');
             // // alert(this._client.data['message']);
-            break;
-          case 'login':
-            if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
-              console.log(this._client.data['message']);
-              
-            } else {
-              console.log('LOGIN OK');
-            }
-            break;
-          case 'get-client':
-            if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
-              console.log(this._client.data['message']);
-            } else {
-              console.log('get-client OK');
-            }
             break;
           case 'shake-hands':
             if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
@@ -169,148 +148,8 @@ export class HeaderComponent implements OnInit {
               this.router.navigate(['/welcome']);
             }
             break;
-          case 'get-profile':
-            if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
-              console.log(this._client.data['message']);
-            } else {
-              // // console.log(this._client.data['user']);
-              const u = JSON.parse(JSON.stringify(c.data['user']));
-              this._currentUserdetail = u;
-              console.log('get user details ok');
-            }
-            break;
-          case 'change-password':
-            if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
-              console.log(this._client.data['message']);
-            } else {
-              alert('change password OK');
-              console.log('change password OK');
-            }
-            break;
-          case 'get-transaction':
-            if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
-              console.log(this._client.data['message']);
-            } else {
-              // // alert('change password OK');
-              console.log('get transaction id ok');
-            }
-            break;
-          case 'check-transaction':
-            if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
-              console.log(this._client.data['message']);
-            } else {
-              // // alert('change password OK');
-              console.log('check transaction id ok');
-            }
-            break;
-          case 'check-forgot':
-            if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
-              console.log(this._client.data['message']);
-            } else {
-              // // alert(this._client.data['message']);
-              console.log('check forgot ok');
-            }
-            break;
-          case 'reset-forgot':
-            if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
-              console.log(this._client.data['message']);
-            } else {
-              console.log('reset forgot ok');
-            }
-            break;
-          case 'submit-forgot':
-            if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
-              console.log(this._client.data['message']);
-            } else {
-              // // alert(this._client.data['message']);
-              this._currentUserdetail = this._client.data['user'];
-              console.log('submit forgot ok');
-            }
-            break;
-          case 'get-user-gui':
-            // console.log('here get user gui ');
-            // // console.log(this._client);
-            if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
-              console.log(this._client.data['message']);
-            } else {
-              // // alert(this._client.data['user'].gui);
-              console.log('get user gui ok');
-            }
-            break;
-          case 'check-phonenumber':
-            if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
-              console.log(this._client.data['message']);
-            } else {
-              // // alert(this._client.data['user'].gui);
-              this._newUser.data = this._client.data;
-              console.log('check phonenumber ok');
-            }
-            break;
-          case 'check-username':
-            if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
-              console.log(this._client.data['message']);
-            } else {
-              this._newUser.data = this._client.data;
-              console.log('chek username ok');
-            }
-            break;
-          case 'check-secret':
-            if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
-              console.log(this._client.data['message']);
-            } else {
-              this._newUser.data = this._client.data;
-              console.log('check secret ok');
-            }
-            break;
-          case 'get-secret':
-            if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
-              console.log(this._client.data['message']);
-            } else {
-              this._newUser.data = this._client.data;
-              console.log('get secret  ok');
-            }
-            break;
-          case 'register':
-            if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
-              console.log(this._client.data['message']);
-            } else {
-              this._newUser.data = this._client.data;
-              console.log('register ok');
-            }
-            break;
-          case 'send-confirm-phone-sms':
-            if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
-              //alert(this._client.data['message']);
-            } else {
-              this._currentUserdetail = this._client.data['user'];
-              //alert('send confirm phone sms ok');
-            }
-            break;
-          case 'check-confirm-phone-sms':
-            if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
-             // alert(this._client.data['message']);
-            } else {
-              this._currentUserdetail = this._client.data['user'];
-              //alert('check confirm phone sms ok');
-            }
-            break;
-            case 'update-confirm-phone-sms':
-            if (this._client.data['message'].toLowerCase().indexOf('error') > -1) {
-              //alert(this._client.data['message']);
-            } else {
-              this._currentUserdetail = this._client.data['user'];
-              this.saveClient();
-              this.logout();
-              this.router.navigate(['/hello-client']);
-              //alert('check confirm phone sms ok');
-            }
-            break;
-          default:
-            break;
         }
-        // console.log(this.heartbeat_interval);
-        // console.log(this._client);
-        // if (evt.data != '.') $('#output').append('<p>'+evt.data+'</p>');
+       
       } else {
         // alert('data empty');
         console.log('data is empty');
@@ -328,42 +167,6 @@ export class HeaderComponent implements OnInit {
     // this._server_event
     const d = event;
     if (d['command'] !== undefined) {
-      // console.log('changed from server');
-      // console.log(d['command'] + d['command2']);
-      switch (d['command']) {
-        case 'notification-changed':
-          if (d['client']['data']['sms'] !== undefined) {
-            console.log('SMS');
-            console.log(d['client']['data']['res'].resultDesc);
-            console.log(d['client']['data']['res'].msisdn);
-          }
-          if (d['client']['data']['topup'] !== undefined) {
-            console.log('topup');
-            console.log(d['client']['data']['res'].resultDesc);
-            console.log(d['client']['data']['res'].msisdn);
-          }
-          if (d['client']['data']['checkbalance'] !== undefined) {
-            console.log('check balance');
-            console.log(d['client']['data']['res'].resultDesc);
-            console.log(d['client']['data']['res'].msisdn);
-          }
-          break;
-        case 'error-changed':
-          console.log(d['client']['data']['message']);
-          break;
-        case 'login-changed':
-          console.log(d['client']['logintoken'] + '   -   ' + d['client']['logintime']);
-          break;
-        case 'message-changed':
-          console.log(d['client']['data']['message']);
-          break;
-        case 'forgot-changed':
-          console.log(d['gui']);
-          break;
-        default:
-          break;
-      }
-      // // console.log(msg);
     }
   }
   readCurrentUserDetail(c: any): any {
@@ -381,8 +184,6 @@ export class HeaderComponent implements OnInit {
   //// SENDING
   showNewMessage() {
     this._client.data.message = 'changed from show message';
-    // this._client.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    // // this.websocketDataServiceService.refreshClient();
     this.websocketDataServiceService.changeMessage(this._client);
   }
   setOtherMessage() {
@@ -395,76 +196,18 @@ export class HeaderComponent implements OnInit {
     this.websocketDataServiceService.setOtherMessage(msg);
   }
   shakeHands() {
-    // this._client.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    // this.websocketDataServiceService.refreshClient();
     this.websocketDataServiceService.shakeHands();
   }
   ping_test() {
-    // this._client.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    // this.websocketDataServiceService.refreshClient();
     this.websocketDataServiceService.ping_test();
     this._client.data.message += ' HERE in app component';
     console.log(this._client);
   }
 
-  login() {
-    // alert(JSON.stringify(this._loginUser));
-    // this._client.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    // this.websocketDataServiceService.refreshClient();
-    this.websocketDataServiceService.login(this._loginUser); // return to this._client
-    this.clearJSONValue(this._loginUser);
-  }
   logout() {
-    // this._client.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    // this.websocketDataServiceService.refreshClient();
     this.websocketDataServiceService.logout();
   }
-  getUserDetails() {
-    // this._client.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    // this.websocketDataServiceService.refreshClient();
-    this.websocketDataServiceService.getUserDetails(this._client.data);
-  }
-  updateUserDetails() {
-    this._currentUserdetail = JSON.parse(this._userDetailsStr);
-    this._currentUserdetail.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    this.websocketDataServiceService.updateUserDetails(this._currentUserdetail); // should be _userDetails
-  }
-  get_user_gui() {
-    // this._client.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    // this.websocketDataServiceService.refreshClient();
-    this.websocketDataServiceService.get_user_gui();
-  }
 
-  changepassword() {
-    this._currentUserdetail.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    this.websocketDataServiceService.changePassword(this._currentUserdetail);
-    this.clearJSONValue(this._currentUserdetail);
-  }
-  register() {
-    this._newUser.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    this.websocketDataServiceService.register(this._newUser);
-    this.clearJSONValue(this._newUser);
-  }
-  getSecret() {
-    this._newUser.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    this.websocketDataServiceService.getSecret(this._newUser);
-  }
-  checkSecret(event: any) {
-    if (this._newUser.data['secret'] !== undefined) {
-      if (this._newUser.data['secret'].length === 6) {
-        this._newUser.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-        this.websocketDataServiceService.checkSecret(this._newUser);
-      }
-    }
-  }
-  checkUsername() {
-    this._newUser.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    this.websocketDataServiceService.checkUsername(this._newUser);
-  }
-  checkPhoneNumber() {
-    this._newUser.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    this.websocketDataServiceService.checkPhoneNumber(this._newUser);
-  }
   getSMSConfirm() {
     if (this._client.logintoken) {
       this._currentUserdetail.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
@@ -500,61 +243,11 @@ export class HeaderComponent implements OnInit {
   }
   goTo(com) {
     console.log(JSON.stringify(this._client));
-    // if (!this._client.gui || this._client.gui === undefined) {
-    //   this._client = this.websocketDataServiceService.getClient();
-    // }
-    // this.websocketDataServiceService.refreshClient();
     this.websocketDataServiceService.setClient(this._client);
     this.router.navigate([com]).then(res => {
-      // this.websocketDataServiceService.stopService();
-      // alert(res);
     }).catch(err => {
       // alert(err);
     });
-  }
-
-  checkForgot(event: any) {
-    let d: any;
-    d = {};
-    d.data = {};
-    d.data.user = {};
-    d.data.user.phonenumber = this._currentUserdetail.phonenumber;
-    d.data.forgot = this._currentUserdetail.forgot;
-    d.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    if (d.data['forgot'] !== undefined) {
-      if (d.data['forgot'].length === 6) {
-        this.websocketDataServiceService.checkForgot(d);
-      }
-    }
-  }
-  resetPassword(event: any) {
-    let d: any;
-    d = {};
-    d.data = {};
-    d.data.user = {};
-    d.data.user.phonenumber = this._currentUserdetail.phonenumber;
-    d.data.forgot = this._currentUserdetail.forgot;
-    d.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    if (d.data['forgot'] !== undefined && d.data.user['phonenumber'] !== undefined) {
-      if (d.data['forgot'].length === 6) {
-        this.websocketDataServiceService.resetPassword(d);
-      }
-    }
-  }
-  getForgotKeys() {
-    let d: any;
-    d = {};
-    d.data = {};
-    d.data.user = {};
-    d.data.user.phonenumber = this._currentUserdetail.phonenumber;
-    d.data.forgot = this._currentUserdetail.forgot;
-
-    d.data.transaction = this.createTransaction(); // NEED TO BE DONE BEOFORE SEND MESSAGE
-    alert(JSON.stringify(d));
-    // alert(JSON.stringify(this._currentUserdetail));
-    if (d.data.user['phonenumber'] !== undefined) {
-      this.websocketDataServiceService.getForgotKeys(d);
-    } else { alert('forgot key is empty'); }
   }
 
   createTransaction() {
